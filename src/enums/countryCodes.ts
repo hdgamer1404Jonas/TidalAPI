18ac962b8f6f86a4b6f73dba275c7153996a4a4d
hdgamer1404Jonas <hdgamer1404Jonas@gmail.com>
2023-11-29 20:24:54 +0100

    All Country codes

--- src/enums/countryCodes.ts
+++ src/enums/countryCodes.ts
@@ -1,8 +1,69 @@
-enum CountryCodes {
-    DE = 'DE',
-    US = 'US',
-}
+// All Countrys Currently supported by Tidal
 
+enum CountryCodes {
+    AL = "AL", // Albania
+    AD = "AD", // Andorra
+    AR = "AR", // Argentina
+    AU = "AU", // Australia
+    AT = "AT", // Austria
+    BE = "BE", // Belgium
+    BA = "BA", // Bosnia and Herzegovina
+    BR = "BR", // Brazil
+    BG = "BG", // Bulgaria
+    CA = "CA", // Canada
+    CL = "CL", // Chile
+    CO = "CO", // Colombia
+    HR = "HR", // Croatia
+    CY = "CY", // Cyprus
+    CZ = "CZ", // Czech Republic
+    DK = "DK", // Denmark
+    DO = "DO", // Dominican Republic
+    EE = "EE", // Estonia
+    FI = "FI", // Finland
+    FR = "FR", // France
+    DE = "DE", // Germany
+    GR = "GR", // Greece
+    HK = "HK", // Hong Kong
+    HU = "HU", // Hungary
+    IS = "IS", // Iceland
+    IE = "IE", // Ireland
+    IL = "IL", // Israel
+    IT = "IT", // Italy
+    JM = "JM", // Jamaica
+    LV = "LV", // Latvia
+    LI = "LI", // Liechtenstein
+    LT = "LT", // Lithuania
+    LU = "LU", // Luxembourg
+    MK = "MK", // Macedonia
+    MY = "MY", // Malaysia
+    MT = "MT", // Malta
+    MX = "MX", // Mexico
+    MC = "MC", // Monaco
+    ME = "ME", // Montenegro
+    NL = "NL", // Netherlands
+    NZ = "NZ", // New Zealand
+    NG = "NG", // Nigeria
+    NO = "NO", // Norway
+    PE = "PE", // Peru
+    PL = "PL", // Poland
+    PT = "PT", // Portugal
+    PR = "PR", // Puerto Rico
+    RO = "RO", // Romania
+    RS = "RS", // Serbia
+    SG = "SG", // Singapore
+    SK = "SK", // Slovakia
+    SI = "SI", // Slovenia
+    ZA = "ZA", // South Africa
+    ES = "ES", // Spain
+    SE = "SE", // Sweden
+    CH = "CH", // Switzerland
+    TH = "TH", // Thailand
+    UG = "UG", // Uganda
+    AE = "AE", // United Arab Emirates
+    GB = "GB", // United Kingdom
+    US = "US", // United States of America
+}
+  
 export {
     CountryCodes
 }
